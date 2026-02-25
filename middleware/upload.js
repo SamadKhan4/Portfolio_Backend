8b0b45c7fbb6f0fdbda47cd5e5d26bf29bcf2ba9
Shishirthakur2416 <samadkhan040204@gmail.com>
2026-02-25 10:50:07 +0530

    3

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,16 +1,17 @@
 const multer = require('multer');
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
-const cloudinary = require('../utils/cloudinary');
 
 // Set up Cloudinary storage
 const storage = new CloudinaryStorage({
-  cloudinary: cloudinary,
+  cloudinary: {
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET
+  },
   params: {
     folder: 'portfolio_projects', // Folder in Cloudinary to store images
     allowed_formats: ['jpeg', 'jpg', 'png', 'webp'],
-    transformation: [
-      { width: 800, height: 600, crop: 'limit' } // Limit size for optimization
-    ]
+    transformation: [{ width: 800, height: 600, crop: 'limit' }] // Limit size for optimization
   },
 });
 
